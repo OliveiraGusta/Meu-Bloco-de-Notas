72bdd89b2805e09141d717477432ae8d4d1c378f
OliveiraGusta <gustavo.oliver456@gmail.com>
2022-02-24 23:54:51 -0300

    comments js

--- Dio.me/Javascript/funcoes.js
+++ Dio.me/Javascript/funcoes.js
@@ -1,11 +1,16 @@
 function getName(){
     return 'Gustavo Oliveira'
 }
+//Função que retorna a String
 
 function logFn(fn){
     console.log(fn());
 }
+//Função pega alguma informaçaõ e exibe
 
+//constante que recebe logFn
 const logFnResult = logFn;
 
+
+//logFnResult recebe a string do get name
 logFnResult(getName);
